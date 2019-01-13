31409890e20e6c1371ac2d9320861a5ae5a5e6c9
Bob Rank <rankrh@gmail.com>
2019-01-12 19:02:40 -0700

    Reorganized

---
+++ geodecode.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 12 18:08:43 2019
+
+@author: Bob
+"""
+
+from urllib.request import urlopen
+import os
+import ssl
+import json
+
+BASE_URL = 'https://maps.googleapis.com/'
+KEY = os.environ.get('GOOGLE_API_KEY')
+ctx = ssl.create_default_context()
+
+
+def GeoDecode(location, output_format='json', language=''):
+    location = location.replace(' ', '+')
+    
+    URL_EXTENSION = 'maps/api/geocode/'
+    url = BASE_URL + URL_EXTENSION + output_format
+    url += '?address=' + location
+    url += '&key=' + KEY
+    
+    data = json.loads(urlopen(url, context=ctx).read().decode('utf-8'))
+    lat = data['results'][0]['geometry']['location']['lat']
+    long = data['results'][0]['geometry']['location']['lng']
+    return (lat, long)
--- haversine.py
+++ haversine.py
@@ -1,36 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jan 12 18:08:43 2019
+Created on Sat Jan 12 18:55:12 2019
 
 @author: Bob
 """
 
 import numpy as np
-from urllib.request import urlopen
-import os
-import ssl
-import json
 
-BASE_URL = 'https://maps.googleapis.com/'
-KEY = os.environ.get('GOOGLE_API_KEY')
-ctx = ssl.create_default_context()
-
-
-def get_lat_long(location, output_format='json', language=''):
-    location = location.replace(' ', '+')
-    
-    URL_EXTENSION = 'maps/api/geocode/'
-    url = BASE_URL + URL_EXTENSION + output_format
-    url += '?address=' + location
-    url += '&key=' + KEY
-    
-    data = json.loads(urlopen(url, context=ctx).read().decode('utf-8'))
-    lat = data['results'][0]['geometry']['location']['lat']
-    long = data['results'][0]['geometry']['location']['lng']
-    return (lat, long)
-    
-
-def haversine(location_1, location_2, unit='miles'):
+def Haversine(location_1, location_2, unit='miles'):
     
     conversion = {'miles': 1609.344,
                   'kilometers': 1000.000,
@@ -56,4 +33,3 @@ def haversine(location_1, location_2, unit='miles'):
     
     d = RADIUS * c
     return d / conversion[unit]
-
